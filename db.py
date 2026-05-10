6d07da9ae34d0b45b676eaa68b7dfbf52ead7e06
mahammasroor123 <mahammasroor29@gmail.com>
2026-05-10 13:28:45 +0500

    testing

--- db.py
+++ db.py
@@ -1,11 +1,28 @@
+# import pyodbc
+# conn_str_ayesha=( "DRIVER={ODBC Driver 18 for SQL Server};"
+#     "SERVER=DESKTOP-QQUVANE\\SQLEXPRESS;"
+#     "DATABASE=Hackathon;"
+#     "Trusted_Connection=yes;"
+#     "TrustServerCertificate=yes;")
+
+# conn = pyodbc.connect(conn_str_ayesha)
+
+# cursor = conn.cursor()
+
+# #testing
+# cursor.execute("SELECT * FROM Teams")
+# print("connected to db: ", cursor.fetchall())
+# # print(pyodbc.drivers())
+
+
 import pyodbc
-conn_str_ayesha=( "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=DESKTOP-QQUVANE\\SQLEXPRESS;"
+conn_str_maham=( "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=MAHAMMASROOR29\\SQLEXPRESS;"
     "DATABASE=Hackathon;"
     "Trusted_Connection=yes;"
     "TrustServerCertificate=yes;")
 
-conn = pyodbc.connect(conn_str_ayesha)
+conn = pyodbc.connect(conn_str_maham)
 
 cursor = conn.cursor()
 
@@ -13,3 +30,4 @@ cursor = conn.cursor()
 cursor.execute("SELECT * FROM Teams")
 print("connected to db: ", cursor.fetchall())
 # print(pyodbc.drivers())
+
