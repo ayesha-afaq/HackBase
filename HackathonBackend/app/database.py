664f655301e4c05df5468bec97a6110e3716c9b4
mahammasroor123 <mahammasroor29@gmail.com>
2026-05-17 18:50:16 +0500

    Update database.py

--- HackathonBackend/app/database.py
+++ HackathonBackend/app/database.py
@@ -1,17 +1,23 @@
 import pyodbc
 
 
-conn_str_ayesha=( "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=DESKTOP-QQUVANE\\SQLEXPRESS;"
-    "DATABASE=Hackathon;"
-    "Trusted_Connection=yes;"
-    "TrustServerCertificate=yes;")
+# conn_str_ayesha=( "DRIVER={ODBC Driver 18 for SQL Server};"
+#     "SERVER=DESKTOP-QQUVANE\\SQLEXPRESS;"
+#     "DATABASE=Hackathon;"
+#     "Trusted_Connection=yes;"
+#     "TrustServerCertificate=yes;")
 
-def get_connection():
+#     conn_str_ayesha=( "DRIVER={ODBC Driver 18 for SQL Server};"
+#     "SERVER=MAHAMMASROOR29\SQLEXPRESS;"
+#     "DATABASE=Hackathon;"
+#     "Trusted_Connection=yes;"
+#     "TrustServerCertificate=yes;")
 
-    conn = pyodbc.connect(conn_str_ayesha)
-    return conn
+# def get_connection():
+
+#     conn = pyodbc.connect(conn_str_ayesha)
+#     return conn
 
 
-
+print (pyodbc.drivers())
 
